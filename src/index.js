450d952f787497e11a284ea3736f424bf08be60d
Samuel Santos-Macbook <samuelfds286@gmail.com>
2021-07-09 15:33:19 -0300

    Finished Profile List

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
-import ProfileList from "./profile_list_screen/profilelist";
+import ProfileList from './profile_list_screen/profilelist'
 
 const Main = () => {
   return <ProfileList />
--- src/profile_list_screen/profilelist.js
+++ src/profile_list_screen/profilelist.js
@@ -1,131 +1,163 @@
-import './profilelist.css';
-import { useState, useEffect } from 'react';
+// ReactJS.
+import React from 'react'
+import {useState, useEffect } from 'react'
+
+// CSS.
+import './profilelist.css'
+
+const initialState = {
+  user : {
+    name: '' 
+  },
+  
+  profileList: [],
+  collapsedList: false
+}
+
+const reducer = (state, action) => {
+  if (action === 'ANIME/TOGGLE_EXPANSED_LIST') {
+    return { ...state, collapsedList: !state.collapsedList }
+  } 
+
+  return state
+}
 
 const ProfileList = () => {
-  const [List_All, SetListAllAnime] = useState([]);
-  const [User_Data, SetDataUser] = useState({});
+  const [state] = React.useReducer(reducer, initialState)
 
+  /** API */
+  const [request_List_Watching, SetListAllAnime] = useState([])
+  const [request_User_Data, SetDataUser] = useState({})
+  
   const getAllUserAnime = async (user) => {
-    const Result_Json = await fetch(`https://api.jikan.moe/v3/user/${user}/animelist/watching`).then((response) => response.json());
-    SetListAllAnime(Result_Json.anime);
+    const Result_Json = await fetch(`https://api.jikan.moe/v3/user/${user}/animelist/watching`).then((response) => response.json())
+    SetListAllAnime(Result_Json.anime)
   };
 
   const getUserData = async (user) => {
-    const Result_Json = await fetch(`https://api.jikan.moe/v3/user/${user}`).then((response) => response.json());
-    SetDataUser(Result_Json);
+    const Result_Json = await fetch(`https://api.jikan.moe/v3/user/${user}`).then((response) => response.json())
+    SetDataUser(Result_Json)
   }
 
   useEffect(() => { 
-    getAllUserAnime('Zillaine'); getUserData('Zillaine');
+    getAllUserAnime('Zillaine'); getUserData('Zillaine')
+  }, [])
   
-    const buttonsEl = document.querySelectorAll('.expand-button');
+  /** State - User Data */
+  state.user.name = request_User_Data.username
+    
+  /** State - Profile List */
+  state.profileList = request_List_Watching
 
-    for (let button of buttonsEl) {
-      button.addEventListener('click', (e) => {
-        let content = document.querySelector('.expand-content')
-        
-        content.classList.toggle('expand-content-show')
-      });
-    }
-  }, []);
-
-  return (
-    <body>
-      <Header userData={User_Data} />
-      <Content list={List_All} />
-      <Footer />
-    </body>
-  );
+  return <>
+    <Header />
+    <Content />
+    <Footer />
+  </>
 }
 
-const Header = (props) => {
-  return (
-    <header>
+const Header = props => {
+  const [state] = React.useReducer(reducer, initialState)
+
+  return <header>
       <div>
         <div>
           <span className='material-icons icon-header-size'>menu</span>
-          <h2>{props.userData.username} Anime List</h2>
+          <h2>{state.user.name} Anime List</h2>
         </div>
         <span className='material-icons icon-header-size'>account_circle</span>
       </div>
-    </header>
-  );
+  </header>
 }
 
 const Content = props => {
-  return (
-    <main>
-      <section id='search'>
-        <div>
-          <label>
-            <input type='text' placeholder='Pesquise'></input>
-          </label>
-          <span className='material-icons'>search</span>
-        </div>
-      </section>  
-      <section>
-        { props.list.map((anime, index) =>( 
-          <ul>
-              <li className='paper' key={index}><ListItem item={anime}/></li>
-          </ul>
-        ))}
-      </section>
-      <script src='Menu.js'></script>
-    </main>
-  );
+  const [state] = React.useReducer(reducer, initialState)
+
+  return <main>
+    <section id='search'>
+      <div>
+        <label>
+          <input type='text' placeholder='Pesquise'></input>
+        </label>
+        <span className='material-icons'>search</span>
+      </div>
+    </section>  
+    <section>
+      { state.profileList.map((anime, index) =>( 
+        <ul key={index}>
+          <li className='paper'><ListItem item={anime} key={anime.mal_id}/></li>
+        </ul>
+      ))}
+    </section>
+  </main>
 }
 
 const ListItem = props => {
-  return (
-    <div>
-      <article className='list-card'>
-        <div>
-          <img src={props.item.image_url} alt={props.item.title} width='200px'/>
-        </div>  
-        <div className='slice-box-card'>
-          <p id='title-card'>{props.item.title}</p>
-          <p id='epsode-number-card'>Número de Episódios: {props.item.total_episodes}</p>
-          <p id='description-card'> 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a risus faucibus, tempus purus sit amet, ultricies nunc. 
-            Duis vestibulum sed nisl sit amet rutrum. Vivamus nec erat efficitur, pretium tellus eu, scelerisque lectus. Donec sollicitudin 
-            venenatis lobortis. Ut elementum a lorem ut suscipit. Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat 
-            ullamcorper fermentum. Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat ullamcorper fermentum.
-            Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat ullamcorper fermentum.
-          </p>
-          <p>Tipo: {props.item.type}</p>
+  const [state, dispatch] = React.useReducer(reducer, initialState)
 
-          <div id='ranking-status'>
-            <p id='rating'>Rating: {props.item.rating}</p>
-            <p id='priority'>Priority: {props.item.priority}</p>
-          </div>
-        </div>
-        
-        <div id='user-actions'>
-          <span className='material-icons icons-treatment'>star_rate</span>
-          <span className='material-icons icons-treatment'>thumb_up</span> 
-          <button className='expand-button'><span className='material-icons icons-treatment'>expand_more</span></button>
-        </div>
-      </article>
-      <div className='expand-content'>
-         <p id='description-card'> 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a risus faucibus, tempus purus sit amet, ultricies nunc. 
-            Duis vestibulum sed nisl sit amet rutrum. Vivamus nec erat efficitur, pretium tellus eu, scelerisque lectus. Donec sollicitudin 
-            venenatis lobortis. Ut elementum a lorem ut suscipit. Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat 
-            ullamcorper fermentum. Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat ullamcorper fermentum.
-            Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat ullamcorper fermentum.
-          </p>
-      </div>
-    </div>
-  );
-} 
-
-const Footer = () => {
-  return (
-    <footer>
+  return <article>
+    <div className='list-card'>
       <div>
+        <img src={props.item.image_url} alt={props.item.title} width='200px'/>
+      </div>  
+      <div className='slice-box-card'>
+        <p id='title-card'>{props.item.title}</p>
+        <p id='epsode-number-card'>Número de Episódios: {props.item.total_episodes}</p>
+        <p className='description-card'> 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a risus faucibus, tempus purus sit amet, ultricies nunc. 
+          Duis vestibulum sed nisl sit amet rutrum. Vivamus nec erat efficitur, pretium tellus eu, scelerisque lectus. Donec sollicitudin 
+          venenatis lobortis. Ut elementum a lorem ut suscipit. Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat 
+          ullamcorper fermentum. Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat ullamcorper fermentum.
+          Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat ullamcorper fermentum.
+        </p>
+        <p>Tipo: {props.item.type}</p>
+
+        <div id='ranking-status'>
+          <p id='rating'>Rating: {props.item.rating}</p>
+          <p id='priority'>Priority: {props.item.priority}</p>
+        </div>
       </div>
-    </footer>
-  );
+        
+      <div id='user-actions'>
+        <span className='material-icons icons-treatment'>star_rate</span>
+        <span className='material-icons icons-treatment'>thumb_up</span> 
+        <ArrowButton rotate={ state.collapsedList.toString() } icon='expand_more' onClick={ () => dispatch('ANIME/TOGGLE_EXPANSED_LIST') } />
+      </div>
+    </div> 
+    <ExpansedList open={ state.collapsedList }/>
+  </article>
+}
+
+const ExpansedList = props => {
+  return<div className={ props.open ? 'expand visible' : 'expand' }>
+    <div className='expanded-content'>
+      <h2>My Review</h2>
+      <p className='description-card'> 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a risus faucibus, tempus purus sit amet, ultricies nunc. 
+        Duis vestibulum sed nisl sit amet rutrum. Vivamus nec erat efficitur, pretium tellus eu, scelerisque lectus. Donec sollicitudin 
+        venenatis lobortis. Ut elementum a lorem ut suscipit. Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat 
+        ullamcorper fermentum. Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat ullamcorper fermentum.
+        Quisque pharetra id nibh in maximus. Fusce at ipsum augue. Nullam eu quam nec erat ullamcorper fermentum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a risus faucibus, tempus purus sit amet, ultricies nunc. 
+        Duis vestibulum sed nisl sit amet rutrum. Vivamus nec erat efficitur, pretium tellus eu, scelerisque lectus. Donec sollicitudin 
+        venenatis lobortis. Ut elementum a lorem ut suscipit.Praesent a risus faucibus, tempus purus sit amet, ultricies nunc. 
+        Duis vestibulum sed nisl sit amet rutrum. Vivamus nec erat efficitur, pretium tellus eu, scelerisque lectus. Donec sollicitudin 
+        venenatis lobortis. Ut elementum a lorem ut suscipit.Vivamus nec erat efficitur, pretium tellus eu, scelerisque lectus. Donec sollicitudin 
+        venenatis lobortis. Ut elementum a lorem ut suscipit.
+      </p>
+    </div>
+  </div>
+}
+
+const ArrowButton = props => (
+  <button className={ (props.rotate === 'true') ? 'expand-button rotate' : 'expand-button' } {...props}><span className='material-icons icons-treatment'>{ props.icon }</span></button>
+)
+
+const Footer = props => {
+  return <footer>
+    <div>
+    </div>
+  </footer>
 }
 
 export default ProfileList

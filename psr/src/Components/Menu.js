10c9b29dce8355a47ab240fb0d79a8d99edccd5d
Marcos Xavier <marcos_mjsx@hotmail.com>
2021-07-09 23:52:08 -0300

    Implementando HomeScreen PSR

--- psr/src/Components/Menu.js
+++
@@ -1,8 +0,0 @@
-let botaoEl = document.querySelector("#alterna-menu");
-let bodyEl = document.querySelector("body");
-
-function alternaMenu() {
-  bodyEl.classList.toggle("menu-visivel");
-}
-
-botaoEl.addEventListener("click", alternaMenu);
